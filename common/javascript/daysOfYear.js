3a284a1f5bd8931aa678b120e49ac32a5f4f7cc3
Sean S <sean.m.a.sullivan@icloud.com>
2019-05-22 14:46:34 -0400

    Add files via upload

---
+++ common/javascript/daysOfYear.js
@@ -0,0 +1,42 @@
+var date = new Date();				// this holds today's date
+var month = date.getMonth() + 1;	// this holds the current month
+var day = date.getDate();			// this holds the current day
+var year = date.getFullYear();		// this holds the current year in 4 digits
+var isLeapYear;						// this is a boolean variable to hold if leap year
+var numbOfDays = 0;
+var daysPerMonth = [31, 28, 31, 30, 31,  30, 31, 31, 30, 31, 30, 31];
+var theDate = month + "/" + day + "/" + year;
+
+// calculating is leap year or not
+const date2 = new Date(this.year, 1, 29);
+isLeapYear = date2.getMonth() === 1
+// end calculation
+
+if (isLeapYear == false)
+{
+	for (var x = 0; x < (month - 1); x++)
+	{
+		numbOfDays = numbOfDays + daysPerMonth[x];
+	}
+}
+else
+{
+	for (var x = 0; x < (month - 1); x++)
+	{
+		numbOfDays = numbOfDays + daysPerMonth[x];
+	}
+	if ((month - 1) > 1)
+	{
+		daysPerMonth++;
+	}
+}
+
+numbOfDays = numbOfDays + day;
+
+addEventListener('load', function(e) 
+{
+	document.querySelector('#title').innerHTML = 'Number of Days Currently in ' + year;
+	document.querySelector('#opening').innerHTML = 'The current date is: ' + theDate;
+	document.querySelector('#test').innerHTML = 'Days in the Year thus far is: ' + numbOfDays;
+
+});
