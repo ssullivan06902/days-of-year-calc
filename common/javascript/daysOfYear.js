8e04c70f9a6229c0e8428e37a3625218ec7022f4
Sean S <sean.m.a.sullivan@icloud.com>
2019-05-23 10:31:09 -0400

    Test

--- common/javascript/daysOfYear.js
+++ common/javascript/daysOfYear.js
@@ -1,16 +1,11 @@
-var date = new Date();				// this holds today's date
-var month = date.getMonth() + 1;	// this holds the current month
-var day = date.getDate();			// this holds the current day
-var year = date.getFullYear();		// this holds the current year in 4 digits
-var isLeapYear;						// this is a boolean variable to hold if leap year
+var date = new Date();					// this holds today's date
+var month = date.getMonth() + 1;		// this holds the current month
+var day = date.getDate();				// this holds the current day
+var year = date.getFullYear();			// this holds the current year in 4 digits
+var isLeapYear = calculateLeapYear();	// this is a boolean variable to hold if leap year
 var numbOfDays = 0;
 var daysPerMonth = [31, 28, 31, 30, 31,  30, 31, 31, 30, 31, 30, 31];
 var theDate = month + "/" + day + "/" + year;
-
-// calculating is leap year or not
-const date2 = new Date(this.year, 1, 29);
-isLeapYear = date2.getMonth() === 1
-// end calculation
 
 if (isLeapYear == false)
 {
@@ -40,3 +35,11 @@ addEventListener('load', function(e)
 	document.querySelector('#test').innerHTML = 'Days in the Year thus far is: ' + numbOfDays;
 
 });
+
+// calculating is leap year or not
+function calculateLeapYear()
+{
+	const date2 = new Date(this.year, 1, 29);
+	return date2.getMonth() === 1
+}
+// end calculation
