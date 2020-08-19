e660403148c75b824d863c8dff37d83922901a79
Sean S <sean.m.a.sullivan@icloud.com>
2020-08-19 09:00:49 -0400

    Update daysOfYear.js
    Adding/Updating  comments as needed.

--- common/javascript/daysOfYear.js
+++ common/javascript/daysOfYear.js
@@ -1,3 +1,9 @@
+/*********************************************************************************************************
+ * Filename: daysOfYear.js                                                                               *
+ * Author: Sean Sullivan                                                                                 *
+ * Version: 1.0.1                                                                                        *
+ *********************************************************************************************************/
+
 var date = new Date();					// this holds today's date
 var month = date.getMonth() + 1;			// this holds the current month
 var day = date.getDate();				// this holds the current day
@@ -7,6 +13,8 @@ var numbOfDays = 0;
 var daysPerMonth = [31, 28, 31, 30, 31,  30, 31, 31, 30, 31, 30, 31];
 var theDate = month + "/" + day + "/" + year;
 
+// Checking to see if we are in a Leap Year and if so, 
+// taking that into account when calculating where we are in the year.
 if (isLeapYear == false)
 {
 	for (var x = 0; x < (month - 1); x++)
@@ -27,7 +35,8 @@ else
 }
 
 numbOfDays = numbOfDays + day;
-//test comment
+
+// Displaying the results of the various calculations.
 addEventListener('load', function(e) 
 {
 	document.querySelector('#title').innerHTML = 'Number of Days Currently in ' + year;
@@ -36,7 +45,7 @@ addEventListener('load', function(e)
 
 });
 
-// calculating is leap year or not
+// This is the function for calculating is leap year or not
 function calculateLeapYear()
 {
 	const date2 = new Date(this.year, 1, 29);
